d6c4b4582dd0bea3491bce37f750787c90f6ea53
wahyusyahputraaaa <wahyu.syahputradumai2021@gmail.com>
2024-05-29 21:54:25 -0700

    push pratikum1

---
+++ praktikum1/array.php
@@ -0,0 +1,11 @@
+<?php 
+
+$fruits = [ "Banana" , "Avocado" ,"Melon"];
+echo $fruits[1];
+
+foreach ($fruits as $fruits){
+    echo $fruits;
+}
+
+
+?> 
---
+++ praktikum1/index.php
@@ -0,0 +1,12 @@
+<?php
+// Variable
+$name = 'Wahyu Syahputra';
+$age = '19';
+$weight = "60";
+
+//panggil variable
+echo "Name:  $name";
+echo "<br> Age: $age";
+echo "<br> weight: $weight";
+
+?>
