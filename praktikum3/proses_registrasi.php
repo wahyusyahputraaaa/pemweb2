8da72671a900412989909fbb380d1382b97e9b5f
wahyusyahputraaaa <wahyu.syahputradumai2021@gmail.com>
2024-05-29 22:00:09 -0700

    push praktikum3

---
+++ praktikum3/proses_registrasi.php
@@ -0,0 +1,12 @@
+<?php
+// Array Numerik - index $ value
+$domisili = ["Jakarta", "Bogor", "Depok", "Tanggerang", "Bekasi"];
+
+// Array Assosiative - key $ value
+$program_studi = [
+    "SI" => "Sistem Informasi",
+    "TI" => "Teknik Informatika",
+    "BD" => "Bisnis Digital",
+];
+
+?>
